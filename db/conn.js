7e37bbca21acf9f7f29b93b9b5727768484e3008
Diogo <contatosdiogo155@gmail.com>
2025-12-22 16:59:01 -0300

    Ajustando conn

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,11 @@
-const { createClient } = require('@supabase/supabase-js');
+const { createClient } = require("@supabase/supabase-js");
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_PASS;
+
+if (!supabaseUrl || !supabaseKey) {
+  console.error("Supabase URL ou Key não encontrada no ambiente!");
+}
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
