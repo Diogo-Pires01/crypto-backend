b6f937f970ca1af2ed9dc25151a4f8f8d0d6f0bd
Diogo <contatosdiogo155@gmail.com>
2025-12-22 17:17:53 -0300

    Validando variaveis de ambiente

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,7 @@
 const { createClient } = require("@supabase/supabase-js");
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_PASS;
+const supabaseKey = process.env.SUPABASE_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
   console.error("Supabase URL ou Key não encontrada no ambiente!");
